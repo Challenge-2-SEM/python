132b9214236f885df967ebada37ca4f53ff441db
hrqmartins <hrqmartins129@gmail.com>
2025-09-19 14:00:53 -0300

    Adiciona função exibir_opcoes() para a exibição das opções e para capturar a escolha do usuário.

--- hc_system.py
+++ hc_system.py
@@ -4,6 +4,7 @@ Henrique Martins - RM 563620
 Henrique Texeira - RM 563088
 Henrique Pacheco - RM 562086
 
+TITULO: Utilizamos o site FSymbols para personalizar o titulo em ASCII ART e simbolos. 
 OS: Utilizamos o módulo OS para limpar o terminal e deixar o programa mais limpo.
 '''
 
@@ -19,6 +20,17 @@ def exibir_titulo():
 ╚═════╝░╚═╝░░╚═╝  ░░░░░░  ╚═════╝░╚═╝╚═════╝░░░░╚═╝░░░╚══════╝╚═╝░░░░░╚═╝╚═╝░░╚═╝
           
 ''')
-    print('-=≡≣ *----------------------------------------------------------------------* ≣≡=-')
+    print('-=≡≣ *======================================================================* ≣≡=-')
     
+def exibir_opcoes():
+    print(f'''
+1. Agendar Teleconsulta
+2. Listar Agendamentos
+3. Reagendar Teleconsulta
+4. Cancelar Agendamento
+5. Sair
+''')
+    usuario_opcao = int(input('Escolha uma opção: '))
+
 exibir_titulo()
+exibir_opcoes()
